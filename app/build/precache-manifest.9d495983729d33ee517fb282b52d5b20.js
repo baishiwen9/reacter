8ad4a6077ddd5fcb7d05c0a92653f66c2aa6efb4
baishiwen <baishiwen@babytree-inc.com>
2020-04-09 17:29:49 +0800

    fix build

---
+++ app/build/precache-manifest.9d495983729d33ee517fb282b52d5b20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd4a7268d0047cfcbd7e45d10675f261",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ed789ace3f3bad36a7c",
+    "url": "/static/css/2.e2c1cc4c.chunk.css"
+  },
+  {
+    "revision": "11323dc640eb1e3bad37",
+    "url": "/static/css/main.7ff9d311.chunk.css"
+  },
+  {
+    "revision": "4ed789ace3f3bad36a7c",
+    "url": "/static/js/2.0c49eeb2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.0c49eeb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11323dc640eb1e3bad37",
+    "url": "/static/js/main.05552cb5.chunk.js"
+  },
+  {
+    "revision": "3f809adccd7b5eb81ce7",
+    "url": "/static/js/runtime-main.6d8ceafa.js"
+  }
+]);
