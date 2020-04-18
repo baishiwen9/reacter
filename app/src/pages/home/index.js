2292bec4e97318b7c5adaf42105d8a6df969bef6
baishiwen <baishiwen@babytree-inc.com>
2020-04-18 21:37:53 +0800

    fix

--- app/src/pages/home/index.js
+++ app/src/pages/home/index.js
@@ -2,36 +2,81 @@ import React, { Component } from 'react';
 import './index.scss';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import Carousel from 'antd-mobile/lib/carousel';
+import 'antd-mobile/lib/carousel/style/css';
 import Tabs from 'antd-mobile/es/tabs';
 import 'antd-mobile/lib/tabs/style/css';
 import Badge from 'antd-mobile/lib/badge';
 import 'antd-mobile/lib/badge/style/css';
-import { getTodayVideo } from './../../services/home';
+import MediaPlay from 'h5-media-player';
+import 'h5-media-player/dist/mediaPlayer.min.css';
+import { getTodayVideoDuanzi, getTodayVideo } from './../../services/home';
 import LoginModel from '../../component/loginModel';
 import { getLoginStatus } from '../../actions';
 
 class Home extends Component {
     constructor(props) {
         super(props);
+        this.tabs = [
+            { title: <Badge>段子</Badge> },
+            { title: <Badge dot>视频</Badge> },
+            { title: <Badge>笑话</Badge> },
+        ];
         this.state = {
-            tabs: [
-                { title: <Badge>段子</Badge> },
-                { title: <Badge text={'今日(20)'}>新闻</Badge> },
-                { title: <Badge dot>笑话</Badge> },
-            ],
+            todayVideoList: [],
             videoDuanzi: [],
             showLogin: false,
+            selectedIndex: 0,
         };
     }
 
     componentDidMount() {
-        getTodayVideo().then(res => {
+        getTodayVideoDuanzi().then(res => {
             if (res) {
                 this.setState({
                     videoDuanzi: res,
                 });
             }
         });
+
+        getTodayVideo().then(res => {
+            console.log(res);
+            if (res && res.length > 0) {
+                const result = [];
+                res.map(item => {
+                    if (item && item.data && item.data.content && item.data.content.type === 'video') {
+                        const { description, duration, playUrl, title, id } = item.data.content.data;
+                        result.push({
+                            description, duration, playUrl, title, id,
+                        });
+                    }
+                });
+                this.setState({
+                    todayVideoList: result,
+                });
+            }
+        });
+    }
+
+    componentDidUpdate() {
+        const { todayVideoList } = this.state;
+        // todayVideoList.map(item => {
+        //     new MediaPlay().initVideo({
+        //         id: 'videoItem_' + item.id,
+        //         src: item.playUrl,
+        //         playingCB: function() {
+        //                 console.log('---开发播放---');
+        //         },
+        //         pauseCB: function() {
+        //                 console.log('---暂停播放---');
+        //         },
+        //         playEndCB: function() {
+        //                 console.log('---视频播放结束---')
+        //         },
+        //         beatTime: 3,
+        //         playingBeatCB: function() {},
+        //     });
+        // })
     }
 
 
@@ -45,12 +90,23 @@ class Home extends Component {
         }
     }
 
+    cardBeforeChange(from, to) {
+
+    }
+
+    cardAfterChange(index) {
+
+    }
+
+
     render() {
-        const { tabs, videoDuanzi } = this.state; 
+        const { videoDuanzi, todayVideoList, selectedIndex } = this.state; 
+        const { tabs } = this; 
         return (
             <div className="homeWrap">
                  <Tabs tabs={tabs}
                     initialPage={0}
+                    swipeable={false}
                     onChange={(tab, index) => { console.log('onChange', index, tab); }}
                     onTabClick={(tab, index) =>  this.tabClick(tab, index)}
                 >
@@ -74,8 +130,26 @@ class Home extends Component {
                             ))
                         }
                     </div>
-                    <div>
-                        222
+                    <div className="tab-list todayVideoList">
+                        <Carousel
+                            dots={false}
+                            autoplay={false}
+                            cellSpacing={16}
+                            slideWidth={0.8}
+                            selectedIndex={selectedIndex}
+                            beforeChange={(from, to) => this.cardBeforeChange(from, to)}
+                            afterChange={index => this.cardAfterChange(index)}
+                        >
+                            {
+                                todayVideoList && todayVideoList.map((item, index) => (
+                                    <div className="videoWrap" key={index}>
+                                        <div className="videoItem" id={`videoItem_${item.id}`}>
+                                            <video src={item.playUrl} controls width="100%" height="420px" />
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                        </Carousel>
                     </div>
                     <div>
                         333
--- app/src/services/home.js
+++ app/src/services/home.js
@@ -3,7 +3,7 @@ import config from '../utils';
 
 const appKey = config.appInfo.APPKEY;
 
-export const getTodayVideo = () => {
+export const getTodayVideoDuanzi = () => {
     return new Promise((resolve, reject) => {
         const url = `https://api.apiopen.top/getJoke?page=1&count=10&type=video`;
         axios.get(url).then(res => {
@@ -28,4 +28,29 @@ export const getTodayVideo = () => {
     })
 };
 
+
+export const getTodayVideo = () => {
+    return new Promise((resolve, reject) => {
+        const url = `https://api.apiopen.top/todayVideo`;
+        axios.get(url).then(res => {
+            if (res && res.status === 200 && res.data) {
+                const { code, result } = res.data;
+                if (code === 200) {
+                    resolve(result);
+                }
+            } else {
+                reject({
+                    code: -1,
+                    msg: '获取每日视频出错~~~',
+                })
+            }
+        }).catch(err => {
+            console.log(err);
+            reject({
+                code: -1,
+                msg: err && err.message
+            })
+        });
+    })
+}
 
